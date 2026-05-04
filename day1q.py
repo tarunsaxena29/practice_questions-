b6a941c7431f84f9966a9fe5fbdc01ab2eb24689
tarunsaxena29 <saxenatarun2008@gmail.com>
2026-05-04 22:15:27 +0530

    Add files via upload

---
+++ day1q.py
@@ -0,0 +1,28 @@
+# 1.write program to input 2 number and print their sum ?
+
+first = input("enter first number:")
+second = input("enter second number:")
+sum = int(first) + int(second)
+print ("sum is",sum)
+
+# 2.write a program to input side of square and print area of square?
+side = input("enter side of square:")
+area = int(side) * int(side)
+print("area of square is:",area)
+
+# 3.write a program to input length and breadth of rectangle and print area of rectangle?
+length = input("enter length of rectangle:")
+breadth = input("enter breadth of rectangle:")
+area = int(length) * int(breadth)
+print("area of rectangle is:",area)
+ 
+ # 4.write to input 2 floats points and print their average?
+first = input("enter first number:")
+second = input("enter second number:")
+average = (float(first) + float(second)) / 2
+print("average is:",average)
+ 
+# 5.write a program to input 2 integers numbers, A and B. print true if A is greater than or equal to B, otherwise print false?
+A = int(input("enter first number:"))
+B = int(input("enter second number:"))
+print(A >= B)

74fd9f6d037be0b99837cff0c79fa1c99027ccb9
Tarun Saxena <saxenatarun2008@gmail.com>
2026-05-05 18:24:24 +0530

    Update day1q.py

--- day1q.py
+++ day1q.py
@@ -1,3 +1,87 @@
+print("hello world")
+print("welcome to python programming")
+print("my name is tarun")
+print("i am learning python programing")
+print (29)
+print (29+29)
+name ="tarun"
+age=18
+iq =999.9 
+#vairable are used to store data in memory and we can use that data later in our program
+print("my name is",name)
+print("my age is:",age)
+print("my iq is:",iq)
+
+# datatypes in python 
+print(type(name))
+print(type(age))
+print(type(iq))
+
+A=None
+print(type(A))
+
+#ARETHMETIC OPERATORS
+a=50
+b=30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+#relational operators
+
+x=80
+y=70
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+# assignment operator 
+z=10
+z+=5
+print(z)
+z-=5
+print(z)
+z*=5
+print(z)
+z/=5
+print(z)
+z//=5
+print(z)
+z%=5
+print(z)
+z**=5
+print(z)
+
+#logical operators
+t=60 
+T=70
+print (not(t>T))
+print("and operator:",T and t)
+print("or operator:",T or t)
+
+#type casting
+q= "100"
+print(type(q))
+q=int(q)
+print(type(q))
+
+#input function
+name = input("enter your name:")
+age = input("enter your age :")
+print("welcome",name)
+print("your age is:",age)
+    
+
+# question practice
+
 # 1.write program to input 2 number and print their sum ?
 
 first = input("enter first number:")

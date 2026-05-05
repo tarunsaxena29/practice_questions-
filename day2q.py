e72921b28f8286e71b08c5aa9cafa6bd08fdc20b
Tarun Saxena <saxenatarun2008@gmail.com>
2026-05-05 18:32:51 +0530

    Update day2q.py

--- day2q.py
+++ day2q.py
@@ -1,3 +1,75 @@
+str1="this is a string"
+str2="this is another string"
+str3="this is another string"
+ 
+#escape sequences
+#1. \n 
+str = "this is a string\nthis is another string"
+print(str)
+#2. \t
+str5 = "this is a string\tthis is another string"
+print(str5)
+
+#basic string operations
+#1. concatenation
+print(str1+str2)
+
+#2. length of string
+print(len(str1))
+
+#3. indexing 
+print(str5[5])
+
+#4. slicing
+print(str5[2:9])
+
+## string functions 
+str7="i am a coder"
+
+#1. endwith
+print(str7.endswith("der"))
+
+#2. startwith
+print(str7.startswith("i am"))
+
+#3. capatalize
+print(str7.capitalize())
+
+#4. rerplace
+print(str7.replace("coder","programmer"))
+
+#5. find
+print(str7.find("a"))
+
+#6. count 
+print(str7.count("a"))
+
+# conditional statements
+ 
+light = "yellow"
+
+if (light == "red"):
+    print("stop")
+elif(light=="yellow"):
+    print("get ready")
+elif(light=="green"):
+    print("go")
+else:
+    print("light is not working")
+
+
+marks = int(input("enter your marks"))
+if(marks>=90):
+   print("grade A")
+elif(marks>=80 and marks<90):
+    print("grade B")
+elif(marks>=70 and marks<80):
+    print("grade C")
+else:
+    print("grade D")
+
+# question practice 
+
 # 1. wap to input user's first name and print its length ?
 
 first_name = input("enter your first name")
